6d9ea159da04387f4d873367a129dfdeb99d6009
Philip Schelin <philip.schelin@student.forsbergsskola.se>
2023-10-04 16:22:58 +0200

    Did gravity, implemented jumping, implemented spikes. Cool, eh?

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0.04f;
+    public float speed = 0.04f; // Field
     void Start()
     {
         
@@ -14,5 +15,21 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         transform.Translate(0, 0, speed);
+
+        bool isKeyWasJustPressed = Input.GetKeyDown(KeyCode.Space); //Checks if Space is pressed
+        if (isKeyWasJustPressed == true)
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            rb.AddForce(0, 500, 0);
+        }
+                
+        
+
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddForce(Physics.gravity * GetComponent<Rigidbody>().mass);
     }
 }
