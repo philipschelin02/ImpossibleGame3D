b22e9d0712bd1e8b6f0bff47b9c7c0fb352853df
Philip Schelin <philip.schelin@student.forsbergsskola.se>
2023-10-04 13:11:01 +0200

    Introduced speed field! (Not the drug!)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0.04f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, speed);
+    }
+}
